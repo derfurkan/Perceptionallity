4a638dbb463512b6c68d1d37c3704d9a21f6e0cc
derfurkan <derfurkan@users.noreply.github.com>
2024-04-19 13:35:19 +0200

    04/19/2024
    Some more menu stuff and a few fixes. Going to add some Animation stuff soon to get a proper title animation in code :cry:

---
+++ src/main/java/de/furkan/perceptionallity/menu/menus/OptionsMenu.java
@@ -0,0 +1,25 @@
+package de.furkan.perceptionallity.menu.menus;
+
+import de.furkan.perceptionallity.menu.Menu;
+
+public class OptionsMenu extends Menu {
+
+    public OptionsMenu(int msPerUpdate) {
+        super(msPerUpdate);
+    }
+
+    @Override
+    public String getMenuName() {
+        return "Options";
+    }
+
+    @Override
+    public void onInit() {
+
+    }
+
+    @Override
+    public void onUpdate() {
+
+    }
+}
